0e605c6a44a2a0b46e1a9edcd0cf42b9f3e6d9eb
Stephen Coles <colessa31@mansfield.edu>
2020-04-18 13:28:38 -0400

    Added Models

---
+++ Ribbonwood-Gaming-Site/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ribbonwood_Gaming_Site.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+
+        // Navigation Properties
+        public int UserID { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Ribbonwood-Gaming-Site/Models/Vendor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ribbonwood_Gaming_Site.Models
+{
+    public class Vendor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        // Navigation Properties
+        public ICollection<Product> Products { get; set; }
+    }
+}
